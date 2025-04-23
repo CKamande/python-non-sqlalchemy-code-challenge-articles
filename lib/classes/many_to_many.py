488709f527a89bf9f95035560de427ee9889de36
CKamande <mungagpi@gmail.com>
2025-04-23 09:22:53 +0000

    DONE

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,87 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        Article.all.append(self)
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        if len(name) == 0:
+            raise Exception("Name must be longer than 0 characters")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Name must be a string")
+        raise Exception("Author name is immutable")
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        topics = list({article.magazine.category for article in self.articles()})
+        return topics if topics else None
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Magazine name must be a string.")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Magazine name must be between 2 and 16 characters.")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Magazine category must be a string.")
+        if len(value.strip()) == 0:
+            raise ValueError("Magazine category cannot be an empty string.")
+        self._category = value
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        author_count = {}
+        for article in self.articles():
+            author = article.author
+            author_count[author] = author_count.get(author, 0) + 1
+        result = [author for author, count in author_count.items() if count > 2]
+        return result if result else None
+
--- lib/testing/magazine_test.py
+++ lib/testing/magazine_test.py
@@ -8,13 +8,20 @@ from classes.many_to_many import Author
 class TestMagazine:
     """Magazine in many_to_many.py"""
 
-    def test_has_name(self):
-        """Magazine is initialized with a name"""
+    def test_name_is_mutable_string(self):
+        """magazine name is of type str and can change"""
         magazine_1 = Magazine("Vogue", "Fashion")
         magazine_2 = Magazine("AD", "Architecture")
 
-        assert magazine_1.name == "Vogue"
-        assert magazine_2.name == "AD"
+        assert isinstance(magazine_1.name, str)
+        assert isinstance(magazine_2.name, str)
+
+        magazine_1.name = "New Yorker"
+        assert magazine_1.name == "New Yorker"
+
+        # Properly indented exception test
+        with pytest.raises(TypeError):
+            magazine_2.name = "2"
 
     def test_name_is_mutable_string(self):
         """magazine name is of type str and can change"""
@@ -28,12 +35,12 @@ class TestMagazine:
         assert magazine_1.name == "New Yorker"
 
         # comment out the next two lines if using Exceptions
-        magazine_2.name = 2
-        assert magazine_2.name == "AD"
+        #magazine_2.name = 2
+        #assert magazine_2.name == "AD"
 
-        # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Magazine(2, "Numbers")
+        #uncomment the next two lines if using Exceptions
+        with pytest.raises(Exception):
+            Magazine(2, "Numbers")
 
     def test_name_len(self):
         """magazine name is between 2 and 16 characters, inclusive"""
@@ -44,20 +51,20 @@ class TestMagazine:
         assert 2 <= len(magazine_2.name) <= 16
 
         # comment out the next two lines if using Exceptions
-        magazine_1.name = "New Yorker Plus X"
-        assert magazine_1.name == "Vogue"
+        #magazine_1.name = "New Yorker Plus X"
+        #assert magazine_1.name == "Vogue"
 
         # comment out the next two lines if using Exceptions
-        magazine_2.name = "A"
-        assert magazine_2.name == "AD"
+        #magazine_2.name = "A"
+        #assert magazine_2.name == "AD"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     magazine_1.name = "New Yorker Plus X"
+        with pytest.raises(Exception):
+            magazine_1.name = "New Yorker Plus X"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     magazine_2.name = "A"
+        with pytest.raises(Exception):
+             magazine_2.name = "A"
 
     def test_has_category(self):
         """Magazine is initialized with a category"""
@@ -81,14 +88,14 @@ class TestMagazine:
         assert isinstance(magazine_1.category, str)
 
         # comment out the next two lines if using Exceptions
-        magazine_2.category = 2
-        assert magazine_2.category == "Architecture"
+        #magazine_2.category = 2
+        #assert magazine_2.category == "Architecture"
         
         assert isinstance(magazine_2.category, str)
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Magazine("GQ", 2)
+        with pytest.raises(Exception):
+             Magazine("GQ", 2)
 
     def test_category_len(self):
         """magazine category has length greater than 0"""
@@ -97,13 +104,13 @@ class TestMagazine:
         assert magazine_1.category != ""
 
         # comment out the next three lines if using Exceptions
-        magazine_1.category = ""
-        assert magazine_1.category == "Fashion"
-        assert magazine_1.category != ""
+        #magazine_1.category = ""
+        #assert magazine_1.category == "Fashion"
+        #assert magazine_1.category != ""
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     magazine_1.category = ""
+        with pytest.raises(Exception):
+             magazine_1.category = ""
 
     def test_has_many_articles(self):
         """magazine has many articles"""
